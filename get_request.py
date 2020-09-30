5a379950d7bcc9ef70b31715765950d8be2bb9bb
Quang Hoang <quang.hd@gmx.de>
2020-09-30 17:24:01 +0200

    Quang created get_request.py & editted main.py"

--- get_request.py
+++ get_request.py
@@ -17,15 +17,12 @@ from urllib.request import urlopen
 from xml.etree import ElementTree as xml_etree
 from lxml import etree as lxml_etree
 
-def main():
-    ## load the XML files per URL to stay up-to-date when changes occur.
-    url = "https://blanco.biomol.uci.edu/mpstruc/listAll/mpstrucTblXml" ## weekly + diff()-Bash 
+## load the XML files per URL to stay up-to-date when changes occur.
+url = "https://blanco.biomol.uci.edu/mpstruc/listAll/mpstrucTblXml" ## weekly + diff()-Bash 
 
-    try:
-        response = requests.get(url)
-        treeFile = xml_etree.fromstring(response.content)
-        print(treeFile)
-    except:
-        print('Cell[2]: Tree coulnd\'t be build, is the Website/Database online?\nCheck https://blanco.biomol.uci.edu/mptopo/mptopoTblXml')
-
-main()
+try:
+    response = requests.get(url)
+    treeFile = xml_etree.fromstring(response.content)
+    print(treeFile)
+except:
+    print('Cell[2]: Tree coulnd\'t be build, is the Website/Database online?\nCheck https://blanco.biomol.uci.edu/mptopo/mptopoTblXml')
