c54602ade0806786e671fea81f0c8fb8874c78bf
Quang Hoang <quang.hd@gmx.de>
2020-09-30 17:23:28 +0200

    Quang created get_request.py & editted main.py"

--- get_request.py
+++ get_request.py
@@ -14,19 +14,18 @@ pd.options.display.min_rows = 20
 import requests
 import urllib
 from urllib.request import urlopen
-import xml.etree.ElementTree as etree
+from xml.etree import ElementTree as xml_etree
 from lxml import etree as lxml_etree
 
 def main():
     ## load the XML files per URL to stay up-to-date when changes occur.
-    all_data = "https://blanco.biomol.uci.edu/mpstruc/listAll/mpstrucTblXml" ## weekly + diff()-Bash 
+    url = "https://blanco.biomol.uci.edu/mpstruc/listAll/mpstrucTblXml" ## weekly + diff()-Bash 
 
     try:
-        response = requests.get(all_data)
-        print(response.data)
-        treeFile = etree.fromstring(response.content)
+        response = requests.get(url)
+        treeFile = xml_etree.fromstring(response.content)
+        print(treeFile)
     except:
         print('Cell[2]: Tree coulnd\'t be build, is the Website/Database online?\nCheck https://blanco.biomol.uci.edu/mptopo/mptopoTblXml')
-
 
 main()
--- main.py
+++ main.py
@@ -1,26 +1,7 @@
 ## In this file the deployment of the dashboard should take place.
 # import all_the_files
 
-import requests
-import urllib
-from urllib.request import urlopen
-import xml.etree.ElementTree as etree
-from lxml import etree as lxml_etree
-
+import get_request
 
 if __name__ == "__main__":
-    ## do stuff
-    ## dashboard = new Class Dashboard
-    ## ... dashboard.serve(someHost)
-    ## load the XML files per URL to stay up-to-date when changes occur.
-
-    all_data = "https://blanco.biomol.uci.edu/mpstruc/listAll/mpstrucTblXml" ## weekly + diff()-Bash 
-
-    try:
-        response = requests.get(all_data)
-        print(response.data)
-        treeFile = etree.fromstring(response.content)
-    except:
-        print('Cell[2]: Tree coulnd\'t be build, is the Website/Database online?\nCheck https://blanco.biomol.uci.edu/mptopo/mptopoTblXml')
-
-main()
+    get_request()
