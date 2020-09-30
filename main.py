cdef17e325db8440f6d2691a121d62d9cc60c1cf
= <=>
2020-09-30 17:13:32 +0200

    test-commit and added main

---
+++ main.py
@@ -0,0 +1,11 @@
+## In this file the deployment of the dashboard should take place.
+# import all_the_files
+
+
+
+if __name__ == "__main__":
+    ## do stuff
+    ## dashboard = new Class Dashboard
+    ## ... dashboard.serve(someHost)
+
+    
