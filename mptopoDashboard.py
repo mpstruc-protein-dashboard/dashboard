ea328a5ad663cdb57abe129121815ab925e1b38d
Quang Hoang <quang.hd@gmx.de>
2020-09-30 17:16:25 +0200

    second commit

--- mptopoDashboard.py
+++ mptopoDashboard.py
@@ -1,5 +1,15 @@
-## In this file the deployment of the dashboard should take place.
-# import all_the_files
+#!/usr/bin/env python
+# coding: utf-8
+# required library imports
+
+import altair as alt
+import pandas as pd 
+
+## pandas options
+
+pd.options.display.max_columns = 60
+pd.options.display.min_rows = 20
+
 
 import requests
 import urllib
@@ -7,13 +17,8 @@ from urllib.request import urlopen
 import xml.etree.ElementTree as etree
 from lxml import etree as lxml_etree
 
-
-if __name__ == "__main__":
-    ## do stuff
-    ## dashboard = new Class Dashboard
-    ## ... dashboard.serve(someHost)
+def main():
     ## load the XML files per URL to stay up-to-date when changes occur.
-
     all_data = "https://blanco.biomol.uci.edu/mpstruc/listAll/mpstrucTblXml" ## weekly + diff()-Bash 
 
     try:
@@ -22,5 +27,6 @@ if __name__ == "__main__":
         treeFile = etree.fromstring(response.content)
     except:
         print('Cell[2]: Tree coulnd\'t be build, is the Website/Database online?\nCheck https://blanco.biomol.uci.edu/mptopo/mptopoTblXml')
+
 
 main()
